a099a9cb87abe8197acb9052b7b5cc61be7a444f
Valentin Bloher <vbloher7@gmail.com>
2022-04-17 11:01:18 +0700

    rename variable user to target_user

--- ssl/apply_new_certs.sh
+++ ssl/apply_new_certs.sh
@@ -3,7 +3,7 @@
 while getopts user:domain:services: flag
 do
     case "${flag}" in
-        user)       USER=$OPTARG;;
+        user)       TARGET_USER=$OPTARG;;
         domain)     DOMAIN=$OPTARG;;
         services)   SERVICES+=("$OPTARG");;
     esac
@@ -11,13 +11,13 @@ done
 shift $((OPTIND -1))
 
 CERTS_DIR="/etc/letsencrypt/live/$DOMAIN"
-NEW_CERTS_DIR="/home/$USER/.ssl"
-USER_GROUP="$(id -ng $USER)"
+NEW_CERTS_DIR="/home/$TARGET_USER/.ssl"
+USER_GROUP="$(id -ng $TARGET_USER)"
 
 cp "$CERTS_DIR/fullchain.pem" "$NEW_CERTS_DIR/server_cert.pem"
 cp "$CERTS_DIR/privkey.pem" "$NEW_CERTS_DIR/server_key.pem"
-chown $USER:$USER_GROUP "$NEW_CERTS_DIR/server_cert.pem"
-chown $USER:$USER_GROUP "$NEW_CERTS_DIR/server_key.pem"
+chown $TARGET_USER:$USER_GROUP "$NEW_CERTS_DIR/server_cert.pem"
+chown $TARGET_USER:$USER_GROUP "$NEW_CERTS_DIR/server_key.pem"
 
 for service in "${SERVICES[@]}"; do
     sudo systemctl restart $service
