43d69e3d7681dd707faef15a6c7d4e5dfd81360e
Valentin Bloher <vbloher7@gmail.com>
2022-07-17 20:36:40 +0300

    add nc

--- logo2.sh
+++ logo2.sh
@@ -1,18 +1,19 @@
-!/bin/bash
+#!/bin/bash
 
 BLUE="\033[34m"
-DASH='\\'
+NC="\033[0m"
+DASH="\\"
 
 echo ""
-echo -e "${BLUE}________________/${DASH}${DASH}${DASH}_________/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}___________________/${DASH}${DASH}${DASH}______________________________________        "
-echo -e "${BLUE} _______________${DASH}/${DASH}${DASH}${DASH}________${DASH}////${DASH}${DASH}${DASH}__________________${DASH}/${DASH}${DASH}${DASH}______________________________________       "
-echo -e "${BLUE}  _______________${DASH}/${DASH}${DASH}${DASH}___________${DASH}/${DASH}${DASH}${DASH}__________________${DASH}/${DASH}${DASH}${DASH}______________________________________      "
-echo -e "${BLUE}   __/${DASH}${DASH}${DASH}____/${DASH}${DASH}${DASH}_${DASH}/${DASH}${DASH}${DASH}___________${DASH}/${DASH}${DASH}${DASH}________/${DASH}${DASH}${DASH}${DASH}${DASH}____${DASH}/${DAS>
-echo -e "${BLUE}    _${DASH}//${DASH}${DASH}${DASH}__/${DASH}${DASH}${DASH}__${DASH}/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}_____${DASH}/${DASH}${DASH}${DASH}______/${DASH}${DA>
-echo -e "${BLUE}     __${DASH}//${DASH}${DASH}${DASH}/${DASH}${DASH}${DASH}___${DASH}/${DASH}${DASH}${DASH}////${DASH}${DASH}${DASH}____${DASH}/${DASH}${DASH}${DASH}_____/${DASH}${DASH}${DASH}__${DASH>
-echo -e "${BLUE}      ___${DASH}//${DASH}${DASH}${DASH}${DASH}${DASH}____${DASH}/${DASH}${DASH}${DASH}__${DASH}/${DASH}${DASH}${DASH}____${DASH}/${DASH}${DASH}${DASH}____${DASH}//${DASH}${DASH}${DASH}>
-echo -e "${BLUE}       ____${DASH}//${DASH}${DASH}${DASH}_____${DASH}/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}___/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}>
-echo -e "${BLUE}        _____${DASH}///______${DASH}/////////___${DASH}/////////_____${DASH}/////_____${DASH}///____${DASH}///____${DASH}//////////__${DASH}///__________"
+echo -e "${BLUE}________________/${DASH}${DASH}${DASH}_________/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}___________________/${DASH}${DASH}${DASH}______________________________________        ${NC}"
+echo -e "${BLUE} _______________${DASH}/${DASH}${DASH}${DASH}________${DASH}////${DASH}${DASH}${DASH}__________________${DASH}/${DASH}${DASH}${DASH}______________________________________       ${NC}"
+echo -e "${BLUE}  _______________${DASH}/${DASH}${DASH}${DASH}___________${DASH}/${DASH}${DASH}${DASH}__________________${DASH}/${DASH}${DASH}${DASH}______________________________________      ${NC}"
+echo -e "${BLUE}   __/${DASH}${DASH}${DASH}____/${DASH}${DASH}${DASH}_${DASH}/${DASH}${DASH}${DASH}___________${DASH}/${DASH}${DASH}${DASH}________/${DASH}${DASH}${DASH}${DASH}${DASH}____${DASH}/${DASH}${DASH}${DASH}_____________/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}___/${DASH}${DASH}/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}__     ${NC}"
+echo -e "${BLUE}    _${DASH}//${DASH}${DASH}${DASH}__/${DASH}${DASH}${DASH}__${DASH}/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}_____${DASH}/${DASH}${DASH}${DASH}______/${DASH}${DASH}${DASH}///${DASH}${DASH}${DASH}__${DASH}/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}____/${DASH}${DASH}${DASH}/////${DASH}${DASH}${DASH}_${DASH}/${DASH}${DASH}${DASH}/////${DASH}${DASH}${DASH}_    ${NC}"
+echo -e "${BLUE}     __${DASH}//${DASH}${DASH}${DASH}/${DASH}${DASH}${DASH}___${DASH}/${DASH}${DASH}${DASH}////${DASH}${DASH}${DASH}____${DASH}/${DASH}${DASH}${DASH}_____/${DASH}${DASH}${DASH}__${DASH}//${DASH}${DASH}${DASH}_${DASH}/${DASH}${DASH}${DASH}/////${DASH}${DASH}${DASH}__/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}__${DASH}/${DASH}${DASH}${DASH}___${DASH}///__   ${NC}"
+echo -e "${BLUE}      ___${DASH}//${DASH}${DASH}${DASH}${DASH}${DASH}____${DASH}/${DASH}${DASH}${DASH}__${DASH}/${DASH}${DASH}${DASH}____${DASH}/${DASH}${DASH}${DASH}____${DASH}//${DASH}${DASH}${DASH}__/${DASH}${DASH}${DASH}__${DASH}/${DASH}${DASH}${DASH}___${DASH}/${DASH}${DASH}${DASH}_${DASH}//${DASH}${DASH}///////___${DASH}/${DASH}${DASH}${DASH}_________  ${NC}"
+echo -e "${BLUE}       ____${DASH}//${DASH}${DASH}${DASH}_____${DASH}/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}___/${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}__${DASH}///${DASH}${DASH}${DASH}${DASH}${DASH}/___${DASH}/${DASH}${DASH}${DASH}___${DASH}/${DASH}${DASH}${DASH}__${DASH}//${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}${DASH}_${DASH}/${DASH}${DASH}${DASH}_________ ${NC}"
+echo -e "${BLUE}        _____${DASH}///______${DASH}/////////___${DASH}/////////_____${DASH}/////_____${DASH}///____${DASH}///____${DASH}//////////__${DASH}///__________${NC}"
 echo ""
 
-sleep 2
+sleep 1
