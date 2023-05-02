009eb467e6d89c37407e94c29b8f5f2cb83a7aca
Valentin Bloher <vbloher7@gmail.com>
2023-05-02 22:21:21 +0700

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "alias ss=\"sudo systemctl\"" >> $HOME/.bash_profile
-
 echo "alias sdr=\"sudo systemctl daemon-reload\"" >> $HOME/.bash_profile
-echo "alias sstop=\"sudo systemctl stop\"" >> $HOME/.bash_profile
-echo "alias sstart=\"sudo systemctl start\"" >> $HOME/.bash_profile
 echo "alias sr=\"sudo systemctl restart\"" >> $HOME/.bash_profile
-echo "alias se=\"sudo systemctl enable\"" >> $HOME/.bash_profile
-echo "alias sd=\"sudo systemctl disable\"" >> $HOME/.bash_profile
 echo "alias j=\"sudo journalctl --no-hostname -o cat -f -u\"" >> $HOME/.bash_profile
+
+echo "alias sd=\"sudo docker \"" >> $HOME/.bash_profile
+echo "alias sdc=\"sudo docker compose\"" >> $HOME/.bash_profile
+echo "alias sdl=\"sudo docker logs\"" >> $HOME/.bash_profile
+echo "alias sdps=\"sudo docker ps -a\"" >> $HOME/.bash_profile
 
 source $HOME/.bash_profile
