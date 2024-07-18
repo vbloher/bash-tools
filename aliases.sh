4c5b25e3bf5e775758ffcf34be4762e248a9fabb
vbloher <vbloher7@gmail.com>
2024-07-18 17:21:59 +0300

    use .profile

--- aliases.sh
+++ aliases.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "alias ss=\"sudo systemctl\"" >> $HOME/.bash_profile
-echo "alias sdr=\"sudo systemctl daemon-reload\"" >> $HOME/.bash_profile
-echo "alias sr=\"sudo systemctl restart\"" >> $HOME/.bash_profile
-echo "alias j=\"sudo journalctl --no-hostname -o cat -f -u\"" >> $HOME/.bash_profile
+echo "alias ss=\"sudo systemctl\"" >> $HOME/.profile
+echo "alias sdr=\"sudo systemctl daemon-reload\"" >> $HOME/.profile
+echo "alias sr=\"sudo systemctl restart\"" >> $HOME/.profile
+echo "alias j=\"sudo journalctl --no-hostname -o cat -f -u\"" >> $HOME/.profile
 
-echo "alias d=\"docker \"" >> $HOME/.bash_profile
-echo "alias sd=\"sudo docker \"" >> $HOME/.bash_profile
-echo "alias sdc=\"sudo docker compose\"" >> $HOME/.bash_profile
-echo "alias sdl=\"sudo docker logs\"" >> $HOME/.bash_profile
-echo "alias sdps=\"sudo docker ps -a\"" >> $HOME/.bash_profile
+echo "alias d=\"docker \"" >> $HOME/.profile
+echo "alias sd=\"sudo docker \"" >> $HOME/.profile
+echo "alias sdc=\"sudo docker compose\"" >> $HOME/.profile
+echo "alias sdl=\"sudo docker logs\"" >> $HOME/.profile
+echo "alias sdps=\"sudo docker ps -a\"" >> $HOME/.profile
 
 ln -s /etc/systemd/system $HOME/services
 
-source $HOME/.bash_profile
+source $HOME/.profile
